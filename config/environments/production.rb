d64324ed4aa3f3926f2e5b89d4b119d6ef137405
Brandon Tilley <brandon@brandontilley.com>
2012-12-03 04:07:36 -0800

    Disable asset compression in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Sandbox::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false # disabling compression for Angular injection ease
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
